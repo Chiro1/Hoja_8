ab6d5a53a6189c867eb87d51c5ad7e1074334ed0
Chiro1 <cris.a.06@hotmail.es>
2015-10-03 19:19:02 -0600

    Update Paciente.java

--- Paciente.java
+++ Paciente.java
@@ -1,30 +1,40 @@
 import java.util.Comparator;
 
 
-public class paciente implements Comparator<paciente>{
+/**
+ * @author Christopher Chiroy, 14411; Diego Sosa, sos14735 
+ *
+ */
+
+public class Paciente implements Comparator<Paciente>{
 	private String nombre;
 	private String descripcion;
 	private String codigo;
 	
-	public paciente(String nombre, String descripcion, String codigo){
+	/**
+	 * post: constructor de un nuevo paciente 
+	 */
+	public Paciente(String nombre, String descripcion, String codigo){
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.codigo = codigo;
 	}
-	//Constructor sin parametros para usar la comparacion
-	public paciente() {}
+
+	/**
+	 * post: constructor sin parametros para usar comparacion 
+	 */
+	public Paciente() {}
 	
+	/**
+	 * @return descripcion del paciente
+	 */
 	public String toString(){
 		return nombre+", "+descripcion+", "+codigo;
-	}
-	
-	public void llenarPacientes(){
-		
 	}
 
 	@Override
 	// Compara prioridad de los pacientes segun el codigo
-	public int compare(paciente paciente1, paciente paciente2) {
+	public int compare(Paciente paciente1, Paciente paciente2) {
 		// TODO Auto-generated method stub
 		int p1 = (int)paciente1.codigo.charAt(0);
 		int p2 = (int)paciente2.codigo.charAt(0);
